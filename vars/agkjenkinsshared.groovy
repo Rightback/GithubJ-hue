3f3971bb0bb148df4f1a0db9b6bbf2925967e0c5
AGK-2 <mike2agk@gmail.com>
2021-04-28 00:12:28 -0400

    updated file

--- vars/agkjenkinsshared.groovy
+++ vars/agkjenkinsshared.groovy
@@ -44,6 +44,7 @@ def call(Map agkpipeline) {
 		      echo agkpipeline.sand
 		      var=agkpipeline.sand
 		      echo var
+		      (echo var)
 		      INSTANCE_URL="https://login.salesforce.com"
                       CLIENT_ID="3MVG9ZF4bs_.MKujkw6ZG8mm2riWYl_WPVCg6Mhj5XjZ0ioVY1heGMLB1ahrkTs9TIZwbZCR5IQMUbkfglBwp"
                       USERNAME="admin2@libertysalesforce.org"
